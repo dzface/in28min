a9549dd7287735e561f8a88aea90e01ba6c021f6
dzface <dzface031@gmail.com>
2024-11-08 22:37:17 +0900

    @ConfigurationProperties를 통해 properties의 설정값 호출하기

--- src/main/java/com/example/in28min/controller/CourseController.java
+++ src/main/java/com/example/in28min/controller/CourseController.java
@@ -1,7 +1,9 @@
 package com.example.in28min.controller;
 
 
+import com.example.in28min.config.CurrencyServiceConfiguration;
 import com.example.in28min.entity.Course;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,13 +12,11 @@ import java.util.List;
 
 @RestController
 public class CourseController {
+    @Autowired
+    private CurrencyServiceConfiguration configuration;
 
-    @RequestMapping("/course")
-    public List<Course> retriveAllCourses(){
-        return Arrays.asList(
-                new Course(1, "Learn AWS", "in28minutes"),
-                new Course(2, "Learn DevOps", "in28minutes")
-
-        );
+    @RequestMapping("/currency-service")
+    public CurrencyServiceConfiguration retriveAllCourses(){
+        return configuration;
     }
 }

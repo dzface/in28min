889203f6e442d93ad4ec6d09062cdb7ebd0fa68b
dzface <dzface031@gmail.com>
2024-11-10 23:34:09 +0900

    롬복말고 starter-web에 있는 slf4j 로거 사용하는 법

--- src/main/java/com/example/in28min/controller/UserController.java
+++ src/main/java/com/example/in28min/controller/UserController.java
@@ -1,5 +1,8 @@
 package com.example.in28min.controller;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,9 +10,11 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class UserController {
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
     @RequestMapping("/login")   // 예제하고 다르게 value를 명시해주어야만 인식
     public String login(@RequestParam(value = "name", required = false) String name, ModelMap model) {
         model.put("name", name);
+        logger.debug("This is name {}",name);
         return "loginJspPage";
     }
 }
